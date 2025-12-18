ae8bb8091952e991193e1f02328c29314c7c6ca3
khushiy05 <khushiyewale150@gmail.com>
2025-12-18 22:07:14 +0530

    functions

---
+++ calculator/script.js
@@ -0,0 +1,28 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        let value = e.target.innerHTML;
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+
+        else if(e.target.innerHTML == 'C'){
+            string =""
+            document.querySelector('input').value = string;
+        }
+
+        else if (value === 'DEL') {
+            string = ""
+            document.querySelector('input').value = string;
+        }
+
+        else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    })
+})
